595c93347d45031eef7a9a88357277a5d689f8ab
luistavaris <luistavaris@hotmail.es>
2022-08-15 03:31:33 -0500

    Categoria-html

---
+++ 06Code/ProjectV2.1/Js/FuncionCategoria.js
@@ -0,0 +1,32 @@
+
+function image(option) {
+    switch (option){
+     
+     case 1:
+         document.getElementById('bigPicture').src = "../ImgCategoria/img1.jpg";
+ 
+ 
+         break;
+     case 2:
+         document.getElementById('bigPicture').src = "../ImgCategoria/img2.jpg";
+         break;
+     case 3:
+         document.getElementById('bigPicture').src = "../ImgCategoria/img3.jpg";
+         break;
+     case 4:
+             document.getElementById('bigPicture').src = "../ImgCategoria/img4.jpg";
+             break;
+     case 5:
+             document.getElementById('bigPicture').src = "../ImgCategoria/img5.jpg";
+             break;
+     case 6:
+             document.getElementById('bigPicture').src = "../ImgCategoria/img6.jpg";
+             break;
+     case 7:
+             document.getElementById('bigPicture').src = "../ImgCategoria/img7.jpg";
+             break;
+
+             
+    }
+ }
+ 
