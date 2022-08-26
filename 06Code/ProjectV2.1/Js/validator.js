f31677cf8c9e16ae3c565cea97b90d8f65135633
carlitosdaniel <cdsalazar5@espe.edu.ec>
2022-08-26 10:08:51 -0500

    opinionsValidator

--- 06Code/ProjectV2.1/Js/validator.js
+++ 06Code/ProjectV2.1/Js/validator.js
@@ -1,34 +1,34 @@
-function validarPasswd () {
-   
+function validarPasswd() {
+
     var p1 = document.getElementById("password").value;
     var p2 = document.getElementById("password2").value;
     var espacios = false;
     var cont = 0;
-  
+
     // Este bucle recorre la cadena para comprobar
     // que no todo son espacios
-      while (!espacios && (cont < p1.length)) {
-          if (p1.charAt(cont) == " ")
-              espacios = true;
-          cont++;
-      }
-     
+    while (!espacios && (cont < p1.length)) {
+        if (p1.charAt(cont) == " ")
+            espacios = true;
+        cont++;
+    }
+
     if (espacios) {
-     alert ("La contraseña no puede contener espacios en blanco");
-     return false;
+        alert("La contraseña no puede contener espacios en blanco");
+        return false;
     }
-     
+
     if (p1.length == 0 || p2.length == 0) {
-     alert("Los campos de la password no pueden quedar vacios");
-     return false;
+        alert("Los campos de la password no pueden quedar vacios");
+        return false;
     }
-     
+
     if (p1 != p2) {
-     alert("Las passwords deben de coincidir");
-     return false;
+        alert("Las passwords deben de coincidir");
+        return false;
     } else {
-     alert("Todo esta correcto");
-     return true; 
+        alert("Todo esta correcto");
+        return true;
     }
 }
 
@@ -38,78 +38,78 @@ function validateName() {
     if (!expRegName.exec(productName.value)) {
         alert("Se recibe solo letras");
         document.getElementById("first_name").value = "";
-       
-    } 
+
+    }
 }
 function validateLastName() {
     var expRegLastName = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
     var lastName = document.getElementById("second_name");
     if (!expRegLastName.exec(lastName.value)) {
         alert("Se recibe solo letras");
-        document.getElementById("second_name").value = ""; 
-       
-    } 
+        document.getElementById("second_name").value = "";
+
+    }
 }
-function validateAddres(){
-    var expRegMessage=/^[A-Za-z0-9\s]+$/;
-    var message= document.getElementById("message");
-    if (!expRegMessage.exec(message.value)){
+function validateAddres() {
+    var expRegMessage = /^[A-Za-z0-9\s]+$/;
+    var message = document.getElementById("message");
+    if (!expRegMessage.exec(message.value)) {
         alert("Se recibe solo letras y números");
-        document.getElementById("message").value=""; // limpia el campo en caso no se ingrese letras o números
-    
+        document.getElementById("message").value = ""; // limpia el campo en caso no se ingrese letras o números
+
     }
 }
 
 
 function validateNumber() {
-    var expRegLastName =  /^[09][0-9]{1,9}$/ ;
+    var expRegLastName = /^[09][0-9]{1,9}$/;
     var number = document.getElementById("cellphone");
     if (!expRegLastName.exec(number.value)) {
         alert("Se recibe solo numeros");
-        document.getElementById("cellphone").value = ""; 
-       
-    } 
-}
-function validateUser(){
-    var expRegMessage=/^[A-Za-z0-9\s]+$/;
-    var message= document.getElementById("usuario");
-    if (!expRegMessage.exec(message.value)){
-        alert("Se recibe solo letras y números");
-        document.getElementById("usuario").value=""; 
-    
+        document.getElementById("cellphone").value = "";
+
     }
 }
-function validateLogin(){
-    var expRegMessage=/^[A-Za-z0-9\s]+$/;
-    var message= document.getElementById("user");
-    if (!expRegMessage.exec(message.value)){
+function validateUser() {
+    var expRegMessage = /^[A-Za-z0-9\s]+$/;
+    var message = document.getElementById("usuario");
+    if (!expRegMessage.exec(message.value)) {
         alert("Se recibe solo letras y números");
-        document.getElementById("user").value=""; 
-    
+        document.getElementById("usuario").value = "";
+
+    }
+}
+function validateLogin() {
+    var expRegMessage = /^[A-Za-z0-9\s]+$/;
+    var message = document.getElementById("user");
+    if (!expRegMessage.exec(message.value)) {
+        alert("Se recibe solo letras y números");
+        document.getElementById("user").value = "";
+
     }
 }
 function validateLoginPasswd() {
-   
+
     var p1 = document.getElementById("password").value;
     var espacios = false;
     var cont = 0;
-  
+
     // Este bucle recorre la cadena para comprobar
     // que no todo son espacios
-      while (!espacios && (cont < p1.length)) {
-          if (p1.charAt(cont) == " ")
-              espacios = true;
-          cont++;
-      }
-     
+    while (!espacios && (cont < p1.length)) {
+        if (p1.charAt(cont) == " ")
+            espacios = true;
+        cont++;
+    }
+
     if (espacios) {
-     alert ("La contraseña no puede contener espacios en blanco");
-     return false;
+        alert("La contraseña no puede contener espacios en blanco");
+        return false;
     }
-     
-    if (p1.length == 0 ) {
-     alert("Los campos de la password no pueden quedar vacios");
-     return false;
+
+    if (p1.length == 0) {
+        alert("Los campos de la password no pueden quedar vacios");
+        return false;
     }
-     
+
 }
